c6cf19dfa26fbecc37966a57f55bb03f359ad9cb
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-01-13 21:04:06 +0100

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,31 @@
+
+function areSentencesSimilar(firstSentence: string[], secondSentence: string[], similarPairs: string[][]): boolean {
+    if (firstSentence.length !== secondSentence.length) {
+        return false;
+    }
+
+    const mapSimilarPairs = new Map<string, Set<string>>();
+    for (let i = 0; i < similarPairs.length; ++i) {
+        if (!mapSimilarPairs.has(similarPairs[i][0])) {
+            mapSimilarPairs.set(similarPairs[i][0], new Set());
+        }
+        mapSimilarPairs.get(similarPairs[i][0]).add(similarPairs[i][1]);
+    }
+
+    for (let i = 0; i < firstSentence.length; ++i) {
+        if (firstSentence[i] === secondSentence[i]) {
+            continue;
+        }
+        if (mapSimilarPairs.has(firstSentence[i])
+            && mapSimilarPairs.get(firstSentence[i]).has(secondSentence[i])) {
+            continue;
+        }
+        if (mapSimilarPairs.has(secondSentence[i])
+            && mapSimilarPairs.get(secondSentence[i]).has(firstSentence[i])) {
+            continue;
+        }
+        return false;
+    }
+
+    return true;
+};
